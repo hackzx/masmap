1a1dd6fd494fd203fa8083ef4a6b21808b14a71b
ZX <hackzx@msn.com>
2020-07-01 11:42:17 +0800

    bug fix

--- masmap.py
+++ masmap.py
@@ -25,12 +25,13 @@ def Location(ip):
 def masscan(ip, rate):
     for x in xrange(0, 3):
         os.system('masscan -p1-65535 --wait 1 --rate=' + rate + ' -oG {tmp} {ip}'.format(tmp='/tmp/tmp_result_' + str(x), ip=ip))
+        os.system('cat /tmp/tmp_result')
 
 
 def selectPorts():
     os.system('cat /tmp/tmp_result_0 /tmp/tmp_result_1 /tmp/tmp_result_2 | sort | uniq > /tmp/tmp_result')
     os.system('sed -i -e \'/#/d\' /tmp/tmp_result')
-    os.system('cat /tmp/tmp_result')
+
     ports = ''
     with open('/tmp/tmp_result') as f:
         for line in f:
@@ -42,21 +43,20 @@ def selectPorts():
 
 
 def nmap(ip, ports):
-    # os.system('nmap -Pn -T5 -sV -A {ip} -p{ports} -oN result'.format(ip=ip, ports=ports))
-    os.system('nmap -Pn -T5 -sV -A {ip} -p{ports} -oN ' + ip + '.result'.format(ip=ip, ports=ports))
+    print('\n[CMD] nmap -Pn -T5 -sV -A {ip} -p{ports} -oN {ip}.result'.format(ip=ip, ports=ports))
+    os.system('nmap -Pn -T5 -sV -A {ip} -p{ports} -oN {ip}.result'.format(ip=ip, ports=ports))
 
 
 if __name__ == '__main__':
 
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print('Usage: python2 ' + sys.argv[0] + ' ip rate')
         sys.exit()
 
     ip = url2ip(sys.argv[1])
     rate = sys.argv[2]
 
-    print('\r')
-    print('IP: ' + ip)
+    print('\nIP: ' + ip + '\r')
 
     masscan(ip, rate)
     ports = selectPorts()
