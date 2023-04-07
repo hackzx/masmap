2e5cc388165938f03838495c19a83e1badd528f2
ZX <hackzx@msn.com>
2023-04-07 17:17:49 +0800

    Stable version, updated fscan and fixed some path mess

--- masmap.py
+++ masmap.py
@@ -1,6 +1,5 @@
 # coding: utf8
 #!/usr/bin/env python3
-import ipaddress
 import sys
 import os
 import re
@@ -8,55 +7,23 @@ import socket
 import subprocess
 from urllib.parse import urlparse
 
+path = '/opt/masmap'
 
-fscanElf = 'bin/fscan'
-masscanElf = 'bin/masscan'
+fscanElf = path + '/bin/fscan'
+masscanElf = path + '/bin/masscan'
 
 
-def getIP(input):
-
-    def url2ip(url):
+def url2ip(url):
+    try:
+        domain = urlparse.urlparse(url).hostname
+        ip = socket.gethostbyname(domain)
+        return ip
+    except:
         try:
-            domain = urlparse(url).hostname
-            ip = socket.gethostbyname(domain)
+            ip = socket.gethostbyname(url)
             return ip
         except:
-            try:
-                ip = socket.gethostbyname(url)
-                return ip
-            except:
-                pass
- 
-    def range2list(ip):
-
-        def ip2num(ip):
-            ip = [int(x) for x in ip.split('.')]
-            return ip[0] << 24 | ip[1] << 16 | ip[2] << 8 | ip[3]
-
-        def num2ip(num):
-            return '%s.%s.%s.%s' % ((num & 0xff000000) >> 24,
-                                    (num & 0x00ff0000) >> 16,
-                                    (num & 0x0000ff00) >> 8,
-                                    (num & 0x000000ff))
-
-        start, end = [ip2num(x) for x in ip.split('-')]
-        return [num2ip(num) for num in range(start, end + 1) if num & 0xff]
-
-    def cidr2list(cidr):
-        return [str(ip) for ip in ipaddress.IPv4Network(cidr)]
-
-    if 'http' in input:
-        return url2ip(input)
-
-    if '/' in input:
-        input = re.sub('\.\d+\/','.0/',input)
-        return cidr2list(input)
-
-    if '-' in input:
-        return range2list(input)
-
-    else:
-        return input
+            pass
 
 
 def location(ip):
@@ -90,7 +57,7 @@ def masscan3(ip):
 
 def fscan(ip, ports):
     # print('\n[CMD] {fscan} -np -nobr -h {ip} -p {ports} -o {ip}.result'.format(fscan=fscanElf, ip=ip, ports=ports))
-    os.system('{fscan} -np -nobr -h {ip} -p {ports} -o {ip}.result'.format(fscan=fscanElf, ip=ip, ports=ports))
+    os.system('{fscan} -np -nobr -h {ip} -p {ports} -o /tmp/{ip}.result'.format(fscan=fscanElf, ip=ip, ports=ports))
 
 
 if __name__ == '__main__':
@@ -99,7 +66,7 @@ if __name__ == '__main__':
         print('Usage: python3 ' + sys.argv[0] + ' ip')
         sys.exit()
 
-    ip = getIP(sys.argv[1])
+    ip = url2ip(sys.argv[1])
 
     location(ip)
 
